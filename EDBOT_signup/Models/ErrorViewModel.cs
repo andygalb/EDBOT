96ec85a1fab0cca5317ec846a8c94f8110545fc5
Andrew Galbraith <andy_galb@hotmail.com>
2020-11-19 16:41:43 +0100

    Initial commit

---
+++ EDBOT_signup/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EDBOT_signup.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ EDBOT_signup/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EDBOT_signup.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Name..")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Class...")]
+        [Display(Name = "Class")]
+        public string ClassName { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Email...")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+    }
+}
